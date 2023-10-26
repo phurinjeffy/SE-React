6a6f8725611bf0ea55b4291969de6db294fc46e7
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-10-26 15:29:54 +0700

    add login

---
+++ src/Login/Login.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./style.css";
+
+export const Login = () => {
+  return (
+<div class="LoginContainer">
+    <div class="Backdrop"></div>
+    <div class="SignUpOverlay">
+        <div class="Icons">
+            <div class="Vector1"></div>
+            <div class="VectorWithBackground"></div>
+        </div>
+        <div class="Frame288">
+            <div class="Frame209">
+                <div class="Frame8">
+                    <div class="LoginText">Login</div>
+                </div>
+            </div>
+            <div class="Frame287">
+                <div class="TextField">
+                    <div class="Frame243_1">
+                        <div class="Label">Your email</div>
+                    </div>
+                    <div class="TextFieldInput"></div>
+                </div>
+                <div class="Frame286">
+                    <div class="TextField">
+                        <div class="Frame243_2">
+                            <div class="Label">Your password</div>
+                        </div>
+                        <div class="PasswordHideSee">
+                            <div class="Icon">
+                                <div class="Group1">
+                                    <div class="VectorWithBackgroundIcon"></div>
+                                    <div class="VectorIcon"></div>
+                                </div>
+                            </div>
+                            <div class="Hide">Hide</div>
+                        </div>
+                        <div class="TextFieldInput"></div>
+                    </div>
+                    <div class="ForgetYourPassword">Forget your password</div>
+                </div>
+                <div class="Button">
+                    <div class="ButtonBackground">
+                        <div class="Frame276">
+                            <div class="LogInText">Log in</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img class="Se11" src="https://via.placeholder.com/88x53" />
+        <div class="SoftwareEngineering">Software Engineering</div>
+    </div>
+</div>
+  );
+};
