d624249d913a68928c90070b758cbb16363784ff
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-11-03 20:56:08 +0700

    navbar beseum

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,8 +33,7 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-profile_notification">
-          <div className="NotificationBg" />
-          <div className="NotificationIcon">
+          <button className="navbar-profile_notification_icon">
             <svg width="25" height="25" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" >
               <g clip-path="url(#clip0_645_54)">
                 <path d="M15 0C13.8147 0 12.8571 0.837891 12.8571 1.875V2.92383C8.0022 3.59766 4.28568 7.27734 4.28568 11.7188V13.6758C4.28568 16.3359 3.24774 18.9199 1.35265 20.9941L0.354878 22.0898C-0.0335147 22.5117 -0.107175 23.0918 0.160682 23.5781C0.428539 24.0645 0.991039 24.375 1.60711 24.375H28.3928C29.0089 24.375 29.5714 24.0645 29.8393 23.5781C30.1071 23.0918 30.0335 22.5117 29.6451 22.0898L28.6473 21C26.7522 18.9199 25.7143 16.3359 25.7143 13.6758V11.7188C25.7143 7.27734 21.9977 3.59766 17.1428 2.92383V1.875C17.1428 0.837891 16.1852 0 15 0ZM15 5.625H15.5357C19.3794 5.625 22.5 8.35547 22.5 11.7188V13.6758C22.5 16.4824 23.4308 19.2188 25.1585 21.5625H4.84149C6.56916 19.2188 7.49997 16.4824 7.49997 13.6758V11.7188C7.49997 8.35547 10.6205 5.625 14.4643 5.625H15ZM19.2857 26.25H15H10.7143C10.7143 27.2461 11.1629 28.2012 11.9665 28.9043C12.7701 29.6074 13.8616 30 15 30C16.1384 30 17.2299 29.6074 18.0334 28.9043C18.837 28.2012 19.2857 27.2461 19.2857 26.25Z" fill="white" />
@@ -45,11 +44,14 @@ const Navbar = () => {
                 </clipPath>
               </defs>
             </svg>
-          </div>
-          <div className="NewUpdateNotification" />
+          </button>
         </div>
 
-        <img className="UserProfile" src="https://via.placeholder.com/50x50" alt="pfp" />
+        <div className="navbar-profile_user">
+          <a href="#">
+            <img src="https://wallpapers-clan.com/wp-content/uploads/2023/05/cool-pfp-02.jpg" alt="pfp" />
+          </a>
+        </div>
       </div>
     </div>
   );
