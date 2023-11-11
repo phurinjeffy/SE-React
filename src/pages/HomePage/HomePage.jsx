170c33163d916c78c61df51bf252e1032c13a422
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-11-11 07:44:17 +0700

    homepage basic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,15 @@ import "./HomePage.css"
 const HomePage = () => {
   return (
     <div className="homePage">
-        <div className='Logo'>
-            <img src="https://se.kmitl.ac.th/assets/se.png" alt="logo" />
-        </div>
+      <div className='top'>
+        <img src="https://www.kmitl.ac.th/sites/default/files/styles/banner_3_1/public/2022-03/banner-faculty-1.jpg?itok=Dejwj13-" alt="top"/>
+      </div>
+      <div className='se'>
+          <img src="https://se.kmitl.ac.th/assets/se.png" alt="logo" />
+      </div>
+      <div className='welcomeText'>
+            Software Engineering, KMITL
+      </div>
     </div>
   )
 }
