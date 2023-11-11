5df5981037d646eef92b3daf36f1b0febff0fbdb
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-11-11 20:19:38 +0700

    follower following update

--- src/components/Follower/Follower.jsx
+++ src/components/Follower/Follower.jsx
@@ -11,35 +11,67 @@ const followersData = [
     status: "mutual",
     profilePic: "https://via.placeholder.com/110x110",
   },
+  {
+    id: 2,
+    name: "User 2",
+    year: "Year 3",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 3,
+    name: "User 3",
+    year: "Year 3",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 4,
+    name: "User 4",
+    year: "Year 1",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 5,
+    name: "User 5",
+    year: "Year 4",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
   // Add more follower data as needed
 ];
 
 const Follower = () => {
-  return (
-    <div className="FollowerBox">
-      <div className="Back">
-        <NavLink to="./..">
-          <img src={backIcon} alt="back" />
-        </NavLink>
-      </div>
-
-      <div className="Title">Follower</div>
-
-      <div className="Account">
-        {followersData.map((follower) => (
-          <div className="Follower" key={follower.id}>
-            <img className="AccountPic" src={follower.profilePic} alt={`profile-${follower.id}`} />
-            <div className="AccountName">{follower.name}</div>
-            <div className="YearsStatus">
-              <div className="YearColor" />
-              <div className="AccountYear">{follower.year}</div>
-              <div className="AccountStatus">{follower.status}</div>
+    return (
+      <div className="FollowerBox">
+        <div className="Back">
+          <NavLink to="./..">
+            <img src={backIcon} alt="back" />
+          </NavLink>
+        </div>
+  
+        <div className="Title">Follower</div>
+  
+        <div className="AccountContainer">
+            <div className="Account">
+                {followersData.map((follower) => (
+                    <div className="Follower" key={follower.id}>
+                    <img className="AccountPic" src={follower.profilePic} alt={`profile-${follower.id}`} />
+                    <div className="AccountInfo">
+                        <div className="AccountName">{follower.name}</div>
+                        <div className="YearsStatus">
+                        <div className="YearColor" />
+                        <div className="AccountYear">{follower.year}</div>
+                        <div className="AccountStatus">{follower.status}</div>
+                        </div>
+                    </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Follower;
--- src/components/Following/Following.jsx
+++ src/components/Following/Following.jsx
@@ -3,42 +3,75 @@ import { NavLink } from "react-router-dom";
 import "./Following.css";
 import backIcon from "../../assets/back.svg";
 
+const followingsData = [
+  {
+    id: 1,
+    name: "User 1",
+    year: "Year 2",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 2,
+    name: "User 2",
+    year: "Year 3",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 3,
+    name: "User 3",
+    year: "Year 3",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 4,
+    name: "User 4",
+    year: "Year 1",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  {
+    id: 5,
+    name: "User 5",
+    year: "Year 4",
+    status: "mutual",
+    profilePic: "https://via.placeholder.com/110x110",
+  },
+  // Add more following data as needed
+];
+
 const Following = () => {
-  return (
-    <div className="EditBox">
+    return (
+      <div className="FollowingBox">
         <div className="Back">
-            <NavLink to="./..">
-                <img src={backIcon} alt="back"/>
-            </NavLink>
+          <NavLink to="./..">
+            <img src={backIcon} alt="back" />
+          </NavLink>
         </div>
-
-        <div className="InputSection">
-            <div className="EditProfilePic" >
-                <img src="https://www.asiamediajournal.com/wp-content/uploads/2022/10/Cute-PFP-for-fb.jpg" />
-            </div>
-
-            <div className="InputField">
-                <input className="Name" placeholder="Name"/>
-            </div>
-            <div className="InputField">
-                <input className="Email" placeholder="Email"/>
-            </div>
-            <div className="InputField">
-                <input className="Bio" placeholder="Bio"/>
-            </div>
-            <div className="InputField">
-                <input className="Contact" placeholder="Contact"/>
-            </div>
-
-            <div className="SaveButton">
-                <button>
-                    Save
-                </button>
+  
+        <div className="Title">Following</div>
+  
+        <div className="AccountContainer">
+            <div className="Account">
+                {followingsData.map((following) => (
+                    <div className="Following" key={following.id}>
+                    <img className="AccountPic" src={following.profilePic} alt={`profile-${following.id}`} />
+                    <div className="AccountInfo">
+                        <div className="AccountName">{following.name}</div>
+                        <div className="YearsStatus">
+                        <div className="YearColor" />
+                        <div className="AccountYear">{following.year}</div>
+                        <div className="AccountStatus">{following.status}</div>
+                        </div>
+                    </div>
+                    </div>
+                ))}
             </div>
         </div>
-
-    </div>
-  );
-};
+      </div>
+    );
+  };
 
 export default Following;
