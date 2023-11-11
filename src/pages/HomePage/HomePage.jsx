451c1a8740e856076b2c29cc8c619c4ba8b869cb
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-11-11 20:48:37 +0700

    homepage update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import "./HomePage.css"
+import { NavLink } from 'react-router-dom'
+import seLogo from "../../assets/se_logo_white.png"
 
 const HomePage = () => {
   return (
     <div className="homePage">
-      <div className='top'>
-        <img src="https://www.kmitl.ac.th/sites/default/files/styles/banner_3_1/public/2022-03/banner-faculty-1.jpg?itok=Dejwj13-" alt="top"/>
-      </div>
-      <div className='se'>
-        <img src="https://se.kmitl.ac.th/assets/se.png" alt="logo" />
-      </div>
-      <div className='welcomeText'>
-            Software Engineering, KMITL
-      </div>
+      <NavLink to="/login" className='homePageSignIn'>
+        <img src={seLogo} alt="se"/>
+      </NavLink>
     </div>
   )
 }
